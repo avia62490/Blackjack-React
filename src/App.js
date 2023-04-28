73522d7818193e900e0b1bdedb764356b2ab7bb8
Alex Via <alexandervia@gmail.com>
2023-04-28 14:24:54 -0500

    reformats deck component, implements dealCards function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [sleeve, setSleeve] = useState([]);
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+
+  function dealCards() {
+    console.log("dealing cards");
+    const card = sleeve.shift();
+    setSleeve([...sleeve]);
+    setPlayerHand([...playerHand, card]);
+    console.log(card);
+    console.log(typeof sleeve);
+  }
   useEffect(() => {
     setSleeve(Deck)
   }, [])
   return (
     <div className="App">
+      <p>Deck</p>
       {sleeve}
+      <button onClick={dealCards}>Deal</button>
+      <p>PLayer</p>
+      {playerHand}
+      <p>Dealer</p>
+      {dealerHand}
     </div>
   );
 }
