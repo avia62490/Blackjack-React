9a3371c3ea9269671826b9933873f255f040df48
Alex Via <alexandervia@gmail.com>
2023-05-02 10:01:46 -0500

    playerHit function draws card from sleeve and deals it to player

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
 
+  // import deck component and set the sleeve to a randomly shuffled set of cards
+  useEffect(() => setSleeve(Deck), []);
+
+  // Initial dealing, 2 cards for Player and Dealer
   function dealCards() {
     console.log("dealing cards");
     for(let i = 0; i < 2; i++) {
@@ -20,15 +24,25 @@ function App() {
     console.log(sleeve.length);
   };
 
-  useEffect(() => setSleeve(Deck), []);
+  // Player hits, gets a card from sleeve
+  function playerHit() {
+    console.log('player hits')
+    let card = sleeve.shift();
+    setSleeve([...sleeve]);
+    setPlayerHand(prevPlayerHand => [...prevPlayerHand, card])
+  }
 
   return (
     <div className="App">
       <p>Deck</p>
       {sleeve}
+      <br></br>
       <button onClick={dealCards}>Deal</button>
       <p>PLayer</p>
       {playerHand}
+      <br></br>
+      <button onClick={playerHit}>Hit</button>
+
       <p>Dealer</p>
       {dealerHand}
     </div>
