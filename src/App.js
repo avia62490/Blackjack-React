5edccac0b6768351f8678c19c9d9c281ae3975c9
Alex Via <alexandervia@gmail.com>
2023-05-05 14:04:14 -0500

    rudimentary hand scoring function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [sleeve, setSleeve] = useState([]);
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
+  const [playerScore, setPLayerScore] = useState(0);
+  const [dealerrScore, setDealerrScore] = useState(0);
 
   // import deck component and set the sleeve to a randomly shuffled set of cards
   useEffect(() => setSleeve(Deck), []);
@@ -23,6 +25,19 @@ function App() {
     }
     console.log(sleeve.length);
   };
+
+  useEffect(() => {
+    const result = playerHand.map((element) => {
+      if(Number.isInteger(Number(element[0]))) {
+        return Number(element[0]);
+      } else if(element[0] === "A") {
+        return 11;
+      } else {
+        return 10;
+      }
+    }).reduce((total, current) => total + current, 0)
+    console.log(result)
+  }, [playerHand, playerScore])
 
   // Player hits, gets a card from sleeve
   function playerHit() {
