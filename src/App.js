70f4a13b5e367a23f9c24018ea5b764dc3de66f1
Alex Via <alexandervia@gmail.com>
2023-05-09 12:33:46 -0500

    find total score for Dealer, adds stay function to signify start of dealer's turn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
   const [playerScore, setPLayerScore] = useState(0);
-  const [dealerrScore, setDealerrScore] = useState(0);
+  const [dealerScore, setDealerScore] = useState(0);
+  const [isDealerTurn, setIsDealerTurn] = useState(false)
 
   // import deck component and set the sleeve to a randomly shuffled set of cards
   useEffect(() => setSleeve(Deck), []);
@@ -15,6 +16,7 @@ function App() {
   // Initial dealing, 2 cards for Player and Dealer
   function dealCards() {
     console.log("dealing cards");
+    setIsDealerTurn(false)
     for(let i = 0; i < 2; i++) {
       let card = sleeve.shift();
       setSleeve([...sleeve]);
@@ -26,7 +28,7 @@ function App() {
     console.log(sleeve.length);
   };
 
-// find total score of hand
+// find total score for Player
   useEffect(() => {
     let isAcePresent = false
     const result = playerHand.map((element) => {
@@ -45,12 +47,36 @@ function App() {
     setPLayerScore(result)
   }, [playerHand, playerScore])
 
+// find total score for Delaer
+  useEffect(() => {
+    let isAcePresent = false
+    const result = dealerHand.map((element) => {
+      if(Number.isInteger(Number(element[0]))) {
+        return Number(element[0]);
+      } else if (element[0] === "A" && isAcePresent) {
+        return 1
+      } else if(element[0] === "A") {
+        isAcePresent = true
+        return 11;
+      } else {
+        return 10;
+      }
+    }).reduce((total, current) => total + current, 0)
+    // console.log("Score is " + result)
+    setDealerScore(result)
+  }, [dealerHand, dealerScore])
+
   // Player hits, gets a card from sleeve
   function playerHit() {
     console.log('player hits')
     let card = sleeve.shift();
     setSleeve([...sleeve]);
     setPlayerHand(prevPlayerHand => [...prevPlayerHand, card])
+  }
+
+  function playerStay() {
+    console.log("player stays")
+    setIsDealerTurn(true);
   }
 
   return (
@@ -65,9 +91,11 @@ function App() {
       Score: {playerScore}
       <br></br>
       <button onClick={playerHit}>Hit</button>
+      <button onClick={playerStay}>Stay</button>
 
       <p>Dealer</p>
       {dealerHand}
+      Score: {dealerScore}
     </div>
   );
 }
